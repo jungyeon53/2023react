6330d86a81bce9a6793e3935c6f3f77db5da5e56
jungyeon <wjddus5533@naver.com>
2023-11-10 12:55:50 +0900

    modify

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
+    const jung = "안녕하세요"
+    function test() {
+      return 100;
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{jung}{test()}</div>
     </div>
   );
 }
